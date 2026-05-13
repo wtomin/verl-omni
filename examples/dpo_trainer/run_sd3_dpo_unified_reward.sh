152ef970e93dc99cf8924d3efa484739abd9a730
Didan Deng <33117903+wtomin@users.noreply.github.com>
2026-05-13 17:12:15 +0800

    restrict the max_sequence_length

--- examples/dpo_trainer/run_sd3_dpo_unified_reward.sh
+++ examples/dpo_trainer/run_sd3_dpo_unified_reward.sh
@@ -26,7 +26,7 @@ python3 -m verl_omni.trainer.diffusion.main_dpo \
     data.val_files=$pickscore_test_path \
     trainer.resume_mode=disable \
     data.train_batch_size=4 \
-    data.max_prompt_length=512 \
+    data.max_prompt_length=256 \
     actor_rollout_ref.model.path=$model_name \
     actor_rollout_ref.actor.diffusion_loss.dpo_beta=2000.0 \
     actor_rollout_ref.rollout.pipeline.height=256 \
